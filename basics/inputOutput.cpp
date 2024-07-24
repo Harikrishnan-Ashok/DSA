4336adc337354cc06652178cbc142083822bdefb
Harikrishnan-Ashok <harikrishnanashok2001@gmail.com>
2024-07-24 16:02:35 +0530

    init

---
+++ basics/inputOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+
+    cout << "Enter the numbers, please:\n";
+    cin >> x >> y;
+
+    cout << "x: " << x << " y: " << y << endl;
+    return 0;
+}
+
